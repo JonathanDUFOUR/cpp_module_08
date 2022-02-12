bacaa4440f51f5e0dd04ce9edf27266d2f967863
jodufour <jodufour@student.42.fr>
2022-02-12 09:40:17 +0000

    ex00 [OK] ex01 [TODO]

--- ex00/private/class/easyfind.tpp
+++ ex00/private/class/easyfind.tpp
@@ -6,7 +6,7 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 20:53:10 by jodufour          #+#    #+#             */
-/*   Updated: 2022/02/11 01:22:25 by jodufour         ###   ########.fr       */
+/*   Updated: 2022/02/12 09:21:21 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ class elementNotFoundException : public std::exception
 public:
 	virtual char const	*what(void) const throw()
 	{
-		return "element not found";
+		return "not found";
 	}
 };
 
@@ -30,9 +30,9 @@ typename T::iterator	easyfind(T &haystack, int const needle)
 	typename T::iterator	iter;
 
 	iter = find(haystack.begin(), haystack.end(), needle);
-	if (*iter == needle)
-		return iter;
-	throw elementNotFoundException();
+	if (iter == haystack.end())
+		throw elementNotFoundException();
+	return iter;
 }
 
 #endif
--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -6,53 +6,107 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 20:37:04 by jodufour          #+#    #+#             */
-/*   Updated: 2022/02/11 01:36:46 by jodufour         ###   ########.fr       */
+/*   Updated: 2022/02/12 09:36:46 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <algorithm>
 #include <cstdlib>
-#include <list>
+#include <deque>
 #include <iostream>
+#include <list>
+#include <map>
+#include <vector>
 #include "class/easyfind.tpp"
 
 static void	__test0(void)
 {
+	std::list<int>				lst;
+	std::list<int>::iterator	iter;
+	int							i;
+
 	try
 	{
-		std::list<int>							lst;
-		std::list<int>::iterator				iter;
-		std::list<int>::const_iterator const	end = lst.end();
-		int										i;
-
-		for (i = 0; i < 10; i++)
+		for (i = 0 ; i < 10 ; ++i)
 			lst.push_back(i);
-		iter = easyfind(lst, 5);
-		for (iter = lst.begin(); iter != end; ++iter)
-			std::cout << &iter << " | " << *iter << std::endl;
-		iter = easyfind(lst, 1);
-		std::cout << std::endl;
-		std::cout << &iter << " | " << *iter << std::endl;
 	}
 	catch (std::exception &e)
 	{
 		std::cout << e.what() << std::endl;
 	}
+	for (iter = lst.begin() ; iter != lst.end() ; ++iter)
+		std::cout << *iter << ": " << &*iter << std::endl;
+	std::cout << std::endl;
+	i = 5;
+	try
+	{
+		iter = easyfind(lst, i);
+		std::cout << *iter << ": " << &*iter << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << i << ": " << e.what() << std::endl;
+	}
 }
 
 static void	__test1(void)
 {
-	
+	std::vector<int>			vec;
+	std::vector<int>::iterator	iter;
+	int							i;
+
+	try
+	{
+		for (i = 0 ; i < 10 ; ++i)
+			vec.push_back(i);
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	for (iter = vec.begin() ; iter != vec.end() ; ++iter)
+		std::cout << *iter << ": " << &*iter << std::endl;
+	std::cout << std::endl;
+	i = 9;
+	try
+	{
+		iter = easyfind(vec, i);
+		std::cout << *iter << ": " << &*iter << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << i << ": " << e.what() << std::endl;
+	}
 }
 
 static void	__test2(void)
 {
-	
-}
+	std::deque<int>				deq;
+	std::deque<int>::iterator	iter;
+	int							i;
 
-static void	__test3(void)
-{
-	
+	try
+	{
+		for (i = 0 ; i < 10 ; ++i)
+			deq.push_back(i);
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	for (iter = deq.begin() ; iter != deq.end() ; ++iter)
+		std::cout << *iter << ": " << &*iter << std::endl;
+	std::cout << std::endl;
+	i = 404;
+	try
+	{
+		iter = easyfind(deq, i);
+		std::cout << *iter << ": " << &*iter << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << i << ": " << e.what() << std::endl;
+	}
 }
 
 int	main(void)
@@ -62,7 +116,5 @@ int	main(void)
 	__test1();
 	std::cout << "-----------------------------------------------" << std::endl;
 	__test2();
-	std::cout << "-----------------------------------------------" << std::endl;
-	__test3();
 	return EXIT_SUCCESS;
 }
